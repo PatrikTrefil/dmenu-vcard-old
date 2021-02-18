2165d5839f184422c493f776f5dcdc92b186e2c0
PatrikTrefil <patrik.trefil@gmail.com>
2021-02-18 15:05:51 +0100

    util fnc file

---
+++ utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def notify(text: str, time="8000") -> str:
+    subprocess.run(["notify-send", "-t", time, text])
+
+def copy_to_clipboard(text: str) -> None:
+    subprocess.run(["xclip", "-selection", "clipboard"],
+                   input=text.encode("UTF-8"))
