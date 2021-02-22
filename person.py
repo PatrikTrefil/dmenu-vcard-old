30d620eef22c1d37b97246611e6728cbc5a71eb1
PatrikTrefil <patrik.trefil@gmail.com>
2021-02-22 10:31:33 +0100

    better organization, added module docstrings

--- person.py
+++ person.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""Module that defines the Person class"""
+
 import unidecode
 
 class Person:
--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+"""Dmenu script for getting information about contacts from vcard files"""
 
-import re
 import os
 import subprocess
-import argparse
 import person
+from vcards import *
 from utils import *
 
 PATH = f"{os.getenv('HOME')}/.contacts/contacts/"
@@ -16,61 +16,10 @@ if query == "":
     exit(1)
 
 
-def get_field(field: str, text: str) -> str:
-    """get information after last semicolon"""
-    REGEX = re.compile(field + ".*:(.*)", re.MULTILINE)
-    res = re.search(REGEX, text)
-    try:
-        return res.group(0)
-    except AttributeError:
-        return ""
-
-
-def get_formatted_name(text: str) -> str:
-    return get_field(r"^FN", text)
-
-
-def get_name(text: str) -> str:
-    return get_field(r"^N", text)
-
-
-def parse_name(unformatted_name: str) -> dict:
-    l = unformatted_name[2:].split(";")
-    return {"first name": l[1], "last name": l[0]}
-
-
-def get_email(text: str) -> str:
-    line = get_field("^EMAIL", text)
-    res = re.search(r":(.*?$)", line)
-    try:
-        return res.group(1)
-    except AttributeError:
-        return ""
-
-
-def get_phone(text: str) -> str:
-    line = get_field("^TEL", text)
-    res = re.search(r":(.*?$)", line)
-    try:
-        return res.group(1).replace(" ", "")
-    except AttributeError:
-        return ""
-
-
-def get_birthday(text: str) -> str:
-    line = get_field("^BDAY", text)
-    res = re.search(r":(.*?$)", line)
-    try:
-        month = res.group(1)[4:6]
-        day = res.group(1)[6:8]
-        year = res.group(1)[:4]
-        return f"{day}.{month}.{year}"
-    except AttributeError:
-        return ""
-
-
+# get the list of files
 files = [f"{PATH}{item}" for item in os.listdir(PATH)]
 
+# list of all people using the Person class
 people = []
 
 for file in files:
